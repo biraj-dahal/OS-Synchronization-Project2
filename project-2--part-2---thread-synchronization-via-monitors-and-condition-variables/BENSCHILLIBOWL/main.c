0c0cf1f74176310483f54370ce279f32ac250010
Kryptonite <102353731+biraj-dahal@users.noreply.github.com>
2024-12-08 01:32:17 -0500

    Update main.c

--- project-2--part-2---thread-synchronization-via-monitors-and-condition-variables/BENSCHILLIBOWL/main.c
+++ project-2--part-2---thread-synchronization-via-monitors-and-condition-variables/BENSCHILLIBOWL/main.c
@@ -1,90 +1,88 @@
 // main.c
-#include "BENSCHILLIBOWL.h"
+#include <stdio.h>
+#include <unistd.h>
 #include <pthread.h>
 #include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
 
-// Global variable for the restaurant
-BENSCHILLIBOWL* bcb;
+#include "BENSCHILLIBOWL.h"
+
+
+#define BENSCHILLIBOWL_SIZE 100
+#define NUM_CUSTOMERS 50
+#define NUM_COOKS 10
+#define ORDERS_PER_CUSTOMER 5
+// #define NUM_CUSTOMERS 5
+// #define NUM_COOKS 2
+// #define ORDERS_PER_CUSTOMER 2
+#define EXPECTED_NUM_ORDERS (NUM_CUSTOMERS * ORDERS_PER_CUSTOMER)
+
+
+BENSCHILLIBOWL *bcb;
 
 void* BENSCHILLIBOWLCustomer(void* tid) {
-    int customer_id = *((int*) tid);
-    
-    // Allocate space for order
-    Order* order = (Order*) malloc(sizeof(Order));
-    
-    // Select random menu item and populate order
-    order->menu_item = PickRandomMenuItem();
-    order->customer_id = customer_id;
-    order->next = NULL;
-    
-    // Add order to restaurant
-    int order_number = AddOrder(bcb, order);
-    printf("Customer #%d added order #%d\n", customer_id, order_number);
-    
+    int customer_id = (int)(long)tid;
+
+    for (int i = 0; i < ORDERS_PER_CUSTOMER; i++) {
+        Order *order = (Order *)malloc(sizeof(Order));
+        order->menu_item = PickRandomMenuItem();
+        order->customer_id = customer_id;
+        order->next = NULL;
+
+        int order_number = AddOrder(bcb, order);
+        printf("Customer #%d placed order #%d: %s\n", customer_id, order_number, order->menu_item);
+    }
+
     return NULL;
 }
 
 void* BENSCHILLIBOWLCook(void* tid) {
-    int cook_id = *((int*) tid);
-    
+    int cook_id = (int)(long)tid;
+    int orders_fulfilled = 0;
+
     while (1) {
-        Order* order = GetOrder(bcb);
-        if (order == NULL) break;
-        
-        // Process order
-        printf("Cook #%d fulfilled order #%d for customer #%d\n", 
-               cook_id, order->order_number, order->customer_id);
+        Order *order = GetOrder(bcb);
+        if (order == NULL) break;  
+
+        printf("Cook #%d fulfilled order #%d: %s for customer #%d\n",
+               cook_id, order->order_number, order->menu_item, order->customer_id);
         free(order);
+        orders_fulfilled++;
     }
-    
+
+    printf("Cook #%d fulfilled %d orders\n", cook_id, orders_fulfilled);
     return NULL;
 }
 
 int main() {
-    // Set random seed
-    srand(time(NULL));
-    
-    // Restaurant parameters
-    int num_customers = 50;
-    int num_cooks = 5;
-    int max_size = 10;
-    
-    // Open restaurant
-    bcb = OpenRestaurant(max_size, num_customers);
-    
-    // Create thread IDs
-    pthread_t customers[num_customers];
-    pthread_t cooks[num_cooks];
-    int customer_ids[num_customers];
-    int cook_ids[num_cooks];
-    
-    // Create customer threads
-    for (int i = 0; i < num_customers; i++) {
-        customer_ids[i] = i + 1;
-        pthread_create(&customers[i], NULL, BENSCHILLIBOWLCustomer, &customer_ids[i]);
+    pthread_t customers[NUM_CUSTOMERS];
+    pthread_t cooks[NUM_COOKS];
+
+    bcb = OpenRestaurant(BENSCHILLIBOWL_SIZE, EXPECTED_NUM_ORDERS);
+
+for (int i = 0; i < NUM_CUSTOMERS; i++) {
+    if (pthread_create(&customers[i], NULL, BENSCHILLIBOWLCustomer, (void *)(long)i) != 0) {
+        perror("Failed to create customer thread");
+        exit(EXIT_FAILURE);
     }
-    
-    // Create cook threads
-    for (int i = 0; i < num_cooks; i++) {
-        cook_ids[i] = i + 1;
-        pthread_create(&cooks[i], NULL, BENSCHILLIBOWLCook, &cook_ids[i]);
+}
+
+for (int i = 0; i < NUM_COOKS; i++) {
+    if (pthread_create(&cooks[i], NULL, BENSCHILLIBOWLCook, (void *)(long)i) != 0) {
+        perror("Failed to create cook thread");
+        exit(EXIT_FAILURE);
     }
-    
-    // Wait for all customers to finish
-    for (int i = 0; i < num_customers; i++) {
+}
+
+
+    for (int i = 0; i < NUM_CUSTOMERS; i++) {
         pthread_join(customers[i], NULL);
     }
-    
-    // Wait for all cooks to finish
-    for (int i = 0; i < num_cooks; i++) {
+
+    for (int i = 0; i < NUM_COOKS; i++) {
         pthread_join(cooks[i], NULL);
     }
-    
-    // Close restaurant
+
     CloseRestaurant(bcb);
-    printf("Restaurant closed.\n");
-    
+
     return 0;
 }
